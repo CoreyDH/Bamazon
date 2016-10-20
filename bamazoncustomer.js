b239ce74dfbc9016e46fe1f7f6dd1f8cf92d401b
Corey H <rewer6@gmail.com>
2016-10-20 08:56:57 -0500

    added db connection

--- bamazoncustomer.js
+++ bamazoncustomer.js
@@ -1,2 +1,11 @@
 var inquirer = require('inquirer');
 var mysql = require('mysql');
+var customerDBKeys = require('./keys/customerKey.js');
+
+var connection = mysql.createConnection(customerDBKeys);
+
+
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+});
